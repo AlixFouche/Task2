dad3e17e4b4e8648dc9e5f145d076c4966cbe786
Alix Fouche <alicefouch@hotmail.co.uk>
2017-02-03 12:10:16 +0000

    Add files via upload

---
+++ Ying Yang/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500,500);
+  background(0);
+}
+
+function draw() {
+  
+  fill(250);
+  rect(500, 0, -250, 500);
+  
+  fill(250);
+  ellipse(125,125,100,100);
+  
+  fill(0);
+  ellipse(375,375,100,100);
+  
+}
